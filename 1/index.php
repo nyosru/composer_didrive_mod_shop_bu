853cecd04853eef0d4e64d37ff42581c1c40dbbb
Сергей <nyos@rambler.ru>
2019-11-25 04:53:11 +0500

    0.0.2

---
+++ 1/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//f\pa($vv['now_mod']);
+
+// \f\pa($_POST);
+
+//if (isset($vv['now_mod']['no_cats']{1})) {
+//    $vv['tpl_0body'] = \f\like_tpl('sh-no.cat', $vv['dir_module_tpl'], $vv['dir_site_tpl']);
+//} else {
+    // $vv['body'] = \f\like_tpl('sh', $vv['dir_module_tpl'], $vv['dir_site_tpl']);
+//}
+    
+//echo 
+$vv['tpl_body'] = dir_site_module_nowlev_tpl.'body.htm';
