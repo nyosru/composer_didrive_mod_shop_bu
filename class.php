1cf14c75332257aeb62c60c5bdc8e610dcc2b09e
Сергей <nyos@rambler.ru>
2019-12-07 10:54:58 +0500

    0.0.3 добавил класс

---
+++ class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+  класс модуля
+ * */
+
+namespace Nyos\mod;
+
+if (!defined('IN_NYOS_PROJECT'))
+    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
+
+class ShopBu {
+
+//     public static $dir_img_server = false;
+
+//    public static function config($db) {
+//        return $re2;
+//    }
+
+}
